d3ae7b051203ddc3175664c63a8cc4e45cfad035
GuidoSeverio <guido.severio@homtail.com>
2023-07-28 00:54:46 -0300

    tarea M4U4

--- M3U2/app.js
+++ M3U2/app.js
@@ -4,10 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/admin/login');
+var novedadesRouter = require('./routes/admin/novedades');
 
 var app = express();
 
@@ -21,8 +25,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'qwertyuiopasdfghjklñ',
+  resave: false,
+  saveUninitialized: true
+}));
+
+secured = async(req, res, next) => {
+  try{
+    console.log(req.session.id_usuario);
+    if(req.session.id_usuario){
+      next();
+    } else {
+      res.redirect('/admin/login');
+    }
+  }catch(error){
+    console.log(error);
+  }
+}
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', secured, novedadesRouter);
 
 app.get('/accesorios', function (req, res){
   res.send('Pagina de Accesorios')
